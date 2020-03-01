3af725b4bba01b55e2bfd7052a60515723131c0c
Hoang Ai Viet <hoangaiviet@Hoangs-MacBook-Pro.local>
2020-03-01 11:43:26 +0100

    modified:   CP1/main_ex.cpp 	modified:   CP1/main_ex.out 	modified:   CP1/main_ex.out.dSYM/Contents/Resources/DWARF/main_ex.out

--- CP1/main_ex.cpp
+++ CP1/main_ex.cpp
@@ -1,4 +1,4 @@
-// Exercise Chapter 1 C Primer 
+// Exercises Chapter 1 C++ Primer, Fifth Edition
 
 #include <iostream>
 
@@ -6,74 +6,82 @@ using namespace std;
 using std::cin;
 using std::cout;
 
-//Ex 1.1 
 
-// int main(){
-//     return 0;
-// }
-
-//Ex 1.2
-
-// int main(){
-//     return -1;
-// }
-
-//Ex 1.3
-
-// int main(){
-//     cout << "Hello, World!!!" << endl;
-// }
-
-//Ex 1.4
-
-// int main(){
-//     cout << "Enter 2 numbers:" << endl;
-//     int v1 = 0;
-//     int v2 = 0;
-//     cin >> v1 >> v2;
-//     cout << "Sum: " << v1 + v2 << endl;
-//     cout << "Product: " << v1 * v2 << endl;
-//     return 0;
-// }
-
-//Ex 1.5, 1.6 not relevant
-
-//Ex 1.7, 1.8 
-
-// int main(){
-//     cout << "/*";
-//     cout << "*/";
-//     cout << /* "*/" */";
-//     cout << /* "*/" /* "/*" */;
-// }
-
-//Ex 1.9 to 1.11
 int main(){
-    int n = 50;
-    while(1){
-        n++;
-        if (n == 100){
-            break;
-        }
-    }
-    cout << n << endl;
-    
-    int m = 10;
-    while(1){
-        m--;
-        if (m==0){
-            break;
-        }
-    }
-    cout << m << endl;
 
-    while(1){
-        cout << m++ << endl;
-        if (m == n){
-            break;
-        }
+    //Ex 1.1 to 1.3
+    //     cout << "Hello, World!!!" << endl;
+    //Ex 1.4
+
+    //     cout << "Enter 2 numbers:" << endl;
+    //     int v1 = 0;
+    //     int v2 = 0;
+    //     cin >> v1 >> v2;
+    //     cout << "Sum: " << v1 + v2 << endl;
+    //     cout << "Product: " << v1 * v2 << endl;
+
+    //Ex 1.5, 1.6 not relevant
+
+    //Ex 1.7, 1.8 
+    //     cout << "/*";
+    //     cout << "*/";
+    //     cout << /* "*/" */";
+    //     cout << /* "*/" /* "/*" */;
+
+    //Ex 1.9 to 1.11
+    //     int n = 50;
+    //     while(1){
+    //         n++;
+    //         if (n == 100){
+    //             break;
+    //         }
+    //     }
+    //     cout << n << endl;
+        
+    //     int m = 10;
+    //     while(1){
+    //         m--;
+    //         if (m==0){
+    //             break;
+    //         }
+    //     }
+    //     cout << m << endl;
+
+    //     while(1){
+    //         cout << m++ << endl;
+    //         if (m == n){
+    //             break;
+    //         }
+    //     }
+
+    // Ex 1.12
+    // 1.12 increase 200 time from -100 to 100. sum = 0
+    // int sum = 0;
+    // for (int i = -100; i <= 100; i++){
+    //     sum += i;
+    // }
+    // cout << sum << endl;
+
+    //Ex 1.13
+    int sum = 0, val = 1;
+    // keep executing the while as long as val is less than or equal to 10
+    for (val = 1; val < 12; val++){
+        sum += val; // assigns sum + val to sum
+        
     }
+    std::cout << "Sum of 1 to 10 inclusive is "
+
+    << sum << std::endl;
+
+    //Ex 1.15
+    // error: used colon, not a semicolon, after endl
+    std::cout << "Read each file." << std::endl;
+    // error: missing quotes around string literal
+    std::cout << "Update master." << std::endl;
+    // error: second output operator is missing
+    std::cout << "Write new master." << std::endl;
+    // error: missing ; on return statement
+
     return 0;
-
-
 }
+
