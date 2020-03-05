bc88c7fdcc6361afc5a6c1b948f596034111a309
Hoang Ai Viet <hoangaiviet@Hoangs-MacBook-Pro.local>
2020-03-05 16:48:51 +0100

    modified:   main_cp2.cpp 	modified:   main_cp2.out 	modified:   main_cp2.out.dSYM/Contents/Resources/DWARF/main_cp2.out

--- CP2/main_cp2.cpp
+++ CP2/main_cp2.cpp
@@ -2,6 +2,57 @@
 
 using namespace std;
 
+int ia = 42;
+
  int main(){
-     cout << "Hello" << endl;
- }
+     //Ex 2.1 2.2 2.3 2.4
+    //  unsigned u = 10, u2 = 42;
+    // std::cout << u2 - u << std::endl;   // normal
+    // std::cout << u - u2 << std::endl;   // unpredicted unsigned?
+    // int i = 10, i2 = 42;
+    // std::cout << i2 - i << std::endl;   // normal
+    // std::cout << i - i2 << std::endl;   // negative
+    // std::cout << i - u << std::endl;    // Mix of unsigned and signed make unpredicted value
+
+    //Ex 2.5
+    // 'a';         character literal
+    // "Hello";     string literal
+    // "\n";        newline 
+    // "\v";        vertical tab
+    // "\t";        horizontal tab
+    // "\\";        backlash
+    // "\b";        backspace
+    // "\a";        alert
+    // "\"";        double quote
+    // "\'";        single quote
+
+    //Ex 2.6 2.7
+
+    //Ex 2.8
+    // cout << "2M \n";
+    // cout << "2\tM\n";
+
+    //Ex 2.9
+    // std::cin >> int input_value;
+    // int i = { 3.14 };
+    // double salary = wage = 9999.99;
+    // int i = 3.14;
+
+    //Ex 2.11 declared, defined, defined.
+
+    //SCOPE
+    //Ex 2.13 always take value in scope first.
+    // int ia = 429;
+    // int j = ia;
+    // cout << j << endl;
+    //Ex 2.14
+    // int x = 100, sum = 0;
+    // for(int x = 0; x !=10; ++x){
+    //     sum +=x;
+    //     cout << x << sum << endl;
+    // }
+
+    //REFERENCE
+    
+ }  
+ 
