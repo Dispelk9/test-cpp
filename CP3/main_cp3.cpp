fabb0c93c0026aed9c9f67be33ebca00a9b105ec
Hoang Ai Viet <hoangaiviet@Hoangs-MacBook-Pro.local>
2020-03-14 11:48:09 +0100

    modified:   CP3/main_cp3.cpp 	modified:   CP3/main_cp3.out 	modified:   CP3/main_cp3.out.dSYM/Contents/Resources/DWARF/main_cp3.out

--- CP3/main_cp3.cpp
+++ CP3/main_cp3.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main(){
-    cout << "h" << endl;
-    return 0;
+    // Chapter 3 STRING and VECTOR
+    // string word;
+    // while(getline(cin,word))
+    // if(word.size() > 10)
+    //     cout << word << endl;
+    // else{
+    //     break;
+    // }
+    // return 0;
+
+    //Ex 3.6
+    // string str;
+    // cin >> str;
+    // char cstr[str.size() + 1];
+    // str.copy(cstr, str.size() + 1);
+    // cstr[str.size()] = '\0';
+    // for(int index = 0; index <= str.size(); index++){
+    //     cstr[index] = 'X';
+    // }
+    // cout << cstr << endl;
+
+    //Ex 3.8
+    string str2;
+    int n = 0;
+    while(cin >> str2){
+        str2[n] = 'X';
+        n++;
+        if(n > str2.size()){
+            break;
+        }
+    }
 }
