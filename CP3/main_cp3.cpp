43b04a4ad3f5220f9f96721c8d1c3f4df99695ab
Hoang Ai Viet <hoangaiviet@Hoangs-MacBook-Pro.local>
2020-03-14 12:18:14 +0100

    modified:   main_cp3.cpp 	modified:   main_cp3.out 	modified:   main_cp3.out.dSYM/Contents/Resources/DWARF/main_cp3.out

--- CP3/main_cp3.cpp
+++ CP3/main_cp3.cpp
@@ -27,13 +27,35 @@ int main(){
     // cout << cstr << endl;
 
     //Ex 3.8
-    string str2;
-    int n = 0;
-    while(cin >> str2){
-        str2[n] = 'X';
-        n++;
-        if(n > str2.size()){
-            break;
+    // string str2;
+    // int n = 0;
+    // while(cin >> str2){
+    //     str2[n] = 'X';
+    //     n++;
+    //     if(n > str2.size()){
+    //         break;
+    //     }
+    // }
+    //Ex 3.12 VECTOR 
+    // vector<string> svec(10, "null");
+
+    //Ex 3.14
+    // int x;
+    // std::vector<int> v;
+    // while(cin >> x){
+    //     v.push_back(x);
+    //     for(std::vector<int>::const_iterator i = v.begin(); i != v.end(); i++){ //print vector
+    //         cout << *i << " ";
+    //     }
+    // }
+
+    //Ex 3.15
+    string s;
+    std::vector<string> sv;
+    while(cin >> s){
+        sv.push_back(s);
+        for(std::vector<string>::iterator a = sv.begin(); a != sv.end(); a++){
+            cout << *a << " ";
         }
     }
 }
